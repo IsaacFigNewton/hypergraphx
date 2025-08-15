2e5328af60fc872829553b1e3e748ac9126c8629
Isaac Rudnick <48895941+IsaacFigNewton@users.noreply.github.com>
2025-08-14 20:54:25 -0700

    copied HypergraphVisualizer

--- hypergraphx/viz/__init__.py
+++ hypergraphx/viz/__init__.py
@@ -3,6 +3,7 @@ from hypergraphx.viz.IHypergraphVisualizer import IHypergraphVisualizer
 
 # Import classes
 from hypergraphx.viz.HypergraphVisualizer import HypergraphVisualizer
+from hypergraphx.viz.DirectedHypergraphVisualizer import DirectedHypergraphVisualizer
 
 # Import functions
 from hypergraphx.viz.draw_communities import draw_communities
